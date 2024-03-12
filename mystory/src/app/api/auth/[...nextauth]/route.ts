d9b6bf1fe6fc1ffa04e723c663bc3c0cd2347721
Ashraf Ben <95255220+BenelhajBen@users.noreply.github.com>
2024-03-12 19:56:41 +0400

    Removed current code base
    Will refactor

--- mystory/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-
-import { authOptions } from "@/lib/auth";
-import NextAuth, { AuthOptions } from "next-auth"
-
-
-const handler = NextAuth(authOptions as AuthOptions)
-
-export { handler as GET, handler as POST}
