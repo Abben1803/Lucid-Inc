e9ba43e1c6dcfc11b047fae73918a1f271e1c6f5
Ashraf Ben <95255220+BenelhajBen@users.noreply.github.com>
2024-03-13 22:28:39 +0400

    Added admin interface
    Admin interface has been added. Only accessible by an isAdmin User

---
+++ src/pages/admin/admin.tsx
@@ -0,0 +1,61 @@
+import { GetServerSidePropsContext } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../app/api/auth/[...nextauth]/route';
+import { Book } from '@prisma/client';
+import { prisma } from '../../lib/prisma';
+
+interface AdminProps {
+  newBooks: Book[];
+}
+
+export default function Admin({ newBooks }: AdminProps) {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <h2>New Books</h2>
+      {newBooks.length > 0 ? (
+        <ul>
+          {newBooks.map((book) => (
+            <li key={book.id}>
+              <h3>{book.title}</h3>
+              <p>Author: {book.userEmail}</p>
+              <p>Created At: {book.createdAt.toLocaleString()}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No new books found.</p>
+      )}
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session || !session.user?.email && !session.user?.isAdmin) {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      },
+    };
+  }
+
+  const newBooks = await prisma.book.findMany({
+    where: {
+      adminReview: {
+        none: {}
+      }
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return {
+    props: {
+      newBooks,
+    },
+  };
+}
