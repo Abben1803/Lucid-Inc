cda8ff0dba61abae9864c61eca46fa4f764ff62c
Ashraf Ben <95255220+BenelhajBen@users.noreply.github.com>
2024-04-12 09:51:02 +0400

    Updated flagged route.

--- src/app/api/flagged/route.ts
+++ src/app/api/flagged/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '../../../lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
