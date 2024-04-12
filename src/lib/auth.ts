33484a9c575682938763f341537dc18876599380
Ashraf Ben <95255220+BenelhajBen@users.noreply.github.com>
2024-04-12 09:40:26 +0400

    Vercel fixes
    Some fixes and some better practices added.

---
+++ src/lib/auth.ts
@@ -0,0 +1,99 @@
+import { prisma } from '../lib/prisma'
+import { compare } from 'bcrypt'
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+
+// https://stackoverflow.com/questions/77637651/authoptions-is-not-a-valid-route-export-field
+
+interface SessionUser {
+    id: string;
+    email: string;
+    isAdmin: boolean;
+    randomKey: string;
+  }
+  
+  interface JWTUser {
+    id: string;
+    email: string;
+    isAdmin: boolean;
+    randomKey: string;
+  }
+  
+  
+export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    providers: [
+        CredentialsProvider({
+        name: 'Sign in',
+        credentials: {
+            email: {
+            label: 'Email',
+            type: 'email',
+            placeholder: 'hello@example.com'
+            },
+            password: { label: 'Password', type: 'password' }
+        },
+        async authorize(credentials): Promise<{ email: string; randomKey: string; id: string; isAdmin: boolean } | null> {
+            if (!credentials?.email || !credentials.password) {
+                return null
+            }
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    email: credentials.email
+                }
+            })
+
+            if (!user) {
+                return null
+            }
+
+            const isPasswordValid = await compare(
+                    credentials.password,
+                    user.password,
+            )
+
+            if (!isPasswordValid) {
+                    return null
+            }
+
+            return {
+                id: user.id +'',
+                email: user.email,
+                randomKey: 'Welcome to MUS',
+                isAdmin: user.isAdmin
+            }
+        }
+        })
+    ],
+    callbacks: {
+        session: ({ session, token }) => {
+        //console.log('Session Callback', { session, token })
+        return {
+            ...session,
+            user: {
+            ...session.user,
+            id: token.id || '',
+            isAdmin: token.isAdmin,
+            randomKey: token.randomKey || ''
+            } as SessionUser
+        }
+        },
+        jwt: ({ token, user }) => {
+        //console.log('JWT Callback', { token, user })
+        if (user) {
+            const u = user as unknown as JWTUser
+            return {
+            ...token,
+            id: u.id,
+            isAdmin: u.isAdmin, // checking whether the user is an admin for some administrator page shenanigans
+            randomKey: u.randomKey
+            } 
+        }
+        return token
+        }
+    }
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,7 +3,7 @@ import "../app/globals.css";
 import Link from "next/link";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from '../app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { useState } from 'react';
 import { useRouter } from "next/router";
 import { prisma } from "../lib/prisma";
