997a2f01523258b9c872976263d9524f7df745b5
Bhavin Desai <b@construwell.com>
2024-04-13 11:07:26 +0400

    added favicon, minor ui edits to dashboard. addedd dashboard info if no story exists. commented out for now.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,7 +3,7 @@ import "../app/globals.css";
 import Link from "next/link";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { getSession } from "next-auth/react";
-import { useState } from 'react';
+import { useState } from "react";
 import { prisma } from "../lib/prisma";
 import styles from "../components/newstory.module.css";
 import AsideComponent from "../components/AsideComponent";
@@ -27,6 +27,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     where: {
       userEmail: userEmail ? userEmail : undefined,
       flagged: null,
+    },
+    orderBy: {
+      createdAt: "desc", // Assuming 'createdAt' is the field you want to sort by
     },
     select: {
       id: true,
@@ -56,8 +59,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   };
 }
 
-const DashboardComponent = ({additionalBooks}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-
+const DashboardComponent = ({
+  additionalBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [isAsideOpen, setIsAsideOpen] = useState(true);
   const toggleAside = () => {
@@ -71,6 +75,31 @@ const DashboardComponent = ({additionalBooks}: InferGetServerSidePropsType<typeo
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const currentBooks = additionalBooks.slice(startIndex, endIndex);
+
+  // if (additionalBooks.length === 0) {
+  //   return (
+  //     <div className="flex h-screen bg-base-200 text-base-content">
+  //       <AsideComponent isOpen={isAsideOpen} toggleAside={toggleAside} />
+  //       <main className={`flex-1 p-6 transition-all duration-300 ${isAsideOpen ? "ml-64" : "ml-0"}`}>
+  //         <div className="mt-6 p-6 shadow-sm rounded-lg mb-8 bg-gradient-to-r from-neutral to-base-100 flex items-center justify-center">
+  //           <div className="text-center">
+  //             <p className="text-lg mb-4">
+  //               Hey young reader, you haven't created any stories yet.
+  //             </p>
+  //             <p className="mb-4">
+  //               Click on 'New Story' on the left side and bring your imagination to life right now!
+  //             </p>
+  //             <Link href="/new-story">
+  //               <a className="btn btn-primary btn-lg">
+  //                 Create a New Story
+  //               </a>
+  //             </Link>
+  //           </div>
+  //         </div>
+  //       </main>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="flex h-screen bg-base-200 text-base-content">
@@ -96,16 +125,12 @@ const DashboardComponent = ({additionalBooks}: InferGetServerSidePropsType<typeo
                   {/* Container with fixed height and hidden overflow */}
                   {book.paragraphs?.[0]?.image?.image ? (
                     <Image
-                    
                       src={book.paragraphs[0].image.image}
                       alt={book.title}
-                      width = {500}
-                      height = {500}
-
+                      width={500}
+                      height={500}
                       className="object-cover w-full h-full transition-transform duration-300 ease-in-out hover:scale-110 rounded-t-lg" // Scale image on hover
                     />
-                    
-                    
                   ) : (
                     <div className="bg-base-300 w-full h-full rounded-t-lg"></div> // Placeholder if no image
                   )}
@@ -134,8 +159,10 @@ const DashboardComponent = ({additionalBooks}: InferGetServerSidePropsType<typeo
       </main>
     </div>
   );
-}
+};
 
-export default function DashboardPage(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function DashboardPage(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
   return <DashboardComponent {...props} />;
 }
